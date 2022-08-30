933cf1789a6b75ce4ffc6bf17d06fbaa34c6d0b2
Sijuade Ajagunna <mahyor.sam@gmail.com>
2022-08-30 22:35:00 +0100

    ft: set up update subscriptions

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,15 +1,48 @@
 import type { NextPage } from "next";
-import { useQuery, useSubscription } from "@apollo/client";
+import {
+  useQuery,
+  useSubscription,
+  SubscribeToMoreOptions,
+} from "@apollo/client";
 import { EVENT_UPDATE, GET_EVENTS } from "src/graphql/schema";
 import { Event } from "src/types";
 import { EventContainer } from "@/components/Events/Container";
 import { Loader } from "@/components/Loader";
+import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
-  const { data, loading } = useQuery(GET_EVENTS);
-  const { data: eventUpdateData } = useSubscription(EVENT_UPDATE);
+  const [d, setD] = useState([]);
+  let { data, loading, subscribeToMore } = useQuery(GET_EVENTS);
 
-  console.log({ eventUpdateData });
+  useEffect(() => {
+    subscribeToMore({
+      document: EVENT_UPDATE,
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData?.data) {
+          return prev;
+        }
+
+        const {
+          data: { eventUpdate },
+        } = subscriptionData;
+
+        // if (eventUpdate) {
+        const events = prev.getEvents?.length ? [...prev.getEvents] : [];
+        const idx = events.findIndex(
+          (e: Event) => e.payload.id === eventUpdate.payload.id
+        );
+
+        if (idx > -1) {
+          events[idx] = eventUpdate;
+        }
+
+        // }
+        return Object.assign({}, prev, {
+          getEvents: events,
+        });
+      },
+    });
+  }, [subscribeToMore]);
 
   return (
     <main className="min-h-screen py-8 bg-bg-blue font-primary">
